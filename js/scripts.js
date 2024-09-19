966a36cca80d51621ad5dc50fe3bf6e75dd07f6b
rezequielf01 <rezequielf01@gmail.com>
2024-09-19 07:30:21 -0600

    loader agregado

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+  window.addEventListener('load', function() {
+    const loader = document.querySelector('.loader-container');
+    
+    loader.style.opacity = '0';
+    loader.style.zIndex = '-1';
+
+  });
